c1a7de6fb860bd3c13d3b69490057364e0159d95
Ivlev A.S <ivlevas@ircoc.vrn.ru>
2024-12-17 17:03:04 +0300

    remove all files for review

--- src/igraphics.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "shapebase.h"
-
-struct IGraphics
-{
-	virtual ~IGraphics() = default;
-	virtual void drawLine(int x0, int y0, int x1, int y1) = 0;
-};
--- src/shapebase.cpp
+++
@@ -1,9 +0,0 @@
-#include "shapebase.h"
-#include <cassert>
-
-ShapeBase::ShapeBase(ShapeID id) : m_id(id) {}
-
-std::string ShapeBase::serialize()
-{
-	return ShapeIdHelpers::toString(m_id);
-}
